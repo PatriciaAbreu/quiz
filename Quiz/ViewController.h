249524abffb1c5e360cddde2d4c6b020c88850de
Patricia Machado de Abreu <Patricia@MacBook-Pro-de-Patricia.local>
2015-02-23 17:23:06 -0300

    Quiz implementado

--- Quiz/ViewController.h
+++ Quiz/ViewController.h
@@ -9,6 +9,17 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *labelPergunta;
+@property (weak, nonatomic) IBOutlet UILabel *labeResposta;
+@property NSArray *pergunta;
+@property NSArray *resposta;
+@property int contadorPerg;
+@property NSArray *imagens;
+
+@property (weak, nonatomic) IBOutlet UIImageView *imagem;
+
+- (IBAction)mostrarPergunta:(id)sender;
+- (IBAction)mostrarResposta:(id)sender;
 
 
 @end
--- Quiz/ViewController.m
+++ Quiz/ViewController.m
@@ -14,9 +14,22 @@
 
 @implementation ViewController
 
+@synthesize labelPergunta, labeResposta, pergunta, resposta, contadorPerg, imagem, imagens;
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor purpleColor];
+    pergunta = @[@"Em qual faculdade estuda?", @"Qual o nome desse projeto?", @"Qual o nome da professora desse projeto? "];
+    
+    resposta = @[@"Universidade Presbiteriana Mackenzie", @"MackMobile", @"Ilana"];
+    
+    imagens = @[[UIImage imageNamed:@"imagem2.png"], [UIImage imageNamed:@"imagem1.jpg"], [UIImage imageNamed:@"imagem3.jpg"]];
+    contadorPerg = -1;
+    [labeResposta setText:@"???"];
+    [labelPergunta setText:nil];
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,4 +37,21 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)mostrarPergunta:(id)sender {
+    [labeResposta setText:@"???"];
+    contadorPerg ++;
+    if (contadorPerg < 3) {
+        [labelPergunta setText:pergunta[contadorPerg]];
+    }else{
+        contadorPerg = 0;
+        [labelPergunta setText:pergunta[contadorPerg]];
+    }
+    
+    
+}
+
+- (IBAction)mostrarResposta:(id)sender {
+    [labeResposta setText:resposta[contadorPerg]];
+    [imagem setImage:imagens[contadorPerg]];
+}
 @end
