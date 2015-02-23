ab32f7a7b688e8d608f1b0d1277b62b84ba695d5
Patricia Machado de Abreu <Patricia@MacBook-Pro-de-Patricia.local>
2015-02-23 14:55:29 -0300

    Initial Commit

---
+++ Quiz/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Quiz
+//
+//  Created by Patricia Machado de Abreu on 23/02/15.
+//  Copyright (c) 2015 Patricia Machado de Abreu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Quiz/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Quiz
+//
+//  Created by Patricia Machado de Abreu on 23/02/15.
+//  Copyright (c) 2015 Patricia Machado de Abreu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
